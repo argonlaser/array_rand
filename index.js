bce2cd56daa391d784833cb3919619b3a6e0aa34
Venkata krishna <vkvenkat94@gmail.com>
2016-06-19 12:31:43 -0700

    Indendation done properly.
    Signed-off-by: Venkata krishna <vkvenkat94@gmail.com>

--- index.js
+++ index.js
@@ -1,49 +1,48 @@
 var exporter = {}
 
 var chooseRandomNumer = function(startindex, endIndex){
-	return Math.floor(Math.random() * (startindex - endIndex)) + endIndex;
+  return Math.floor(Math.random() * (startindex - endIndex)) + endIndex;
 }
 // Can we use an object instead of startindex and max index???
 exporter.getRandomNumberInRangeSync = function(array, count, startIndex, endIndex) {
   if(startIndex < 0 || startIndex > array.length - 1) {
-			throw new Error('Invalid startIndex value'); 
-	}
-	if (endIndex < 0 || endIndex > array.length - 1) {
-			throw new Error('Invalid endIndex value');
-	}
-	if (count < startIndex || count > (endIndex - startIndex) + 1) {
- 			throw new Error('Invalid count value');
-	}
+    throw new Error('Invalid startIndex value'); 
+  }
+  if (endIndex < 0 || endIndex > array.length - 1) {
+    throw new Error('Invalid endIndex value');
+  }
+  if (count < startIndex || count > (endIndex - startIndex) + 1) {
+    throw new Error('Invalid count value');
+  }
   var resultList = [];
-	var temp, randomIndex;
-	while(count > 0 && startIndex <= endIndex) {
-		randomIndex = chooseRandomNumer(startIndex, endIndex);
-		// Swap the last and the current element
-		temp = array[randomIndex];
-		array[randomIndex] = array[endIndex];
-		array[endIndex] = temp;
+  var temp, randomIndex;
+  while(count > 0 && startIndex <= endIndex) {
+    randomIndex = chooseRandomNumer(startIndex, endIndex);
+    // Swap the last and the current element
+    temp = array[randomIndex];
+    array[randomIndex] = array[endIndex];
+    array[endIndex] = temp;
 
-		// Push the last element into the resultList
-		resultList.push(temp)
+    // Push the last element into the resultList
+    resultList.push(temp)
 
-		/* Ignore the last element and find next random
-		 * element from the remaining         
-		 */
-		count--;
-		endIndex--;
-	   
-	}
-	return resultList;
+    /* Ignore the last element and find next random
+     * element from the remaining         
+     */
+    count--;
+    endIndex--;
+  }
+  return resultList;
 };
 exporter.getRandomNumberInRange = function(array, count, startIndex, endIndex, callback) {
-	var resultList = [];
-	try{
-			resultList = exporter.getRandomNumberInRangeSync(arr, count, startindex, endIndex);
-			callback(null, resultList);
-	}
-	catch(err) {
-			callback(err, resultList);
-	}
+  var resultList = [];
+  try{
+    resultList = exporter.getRandomNumberInRangeSync(arr, count, startindex, endIndex);
+    callback(null, resultList);
+  }
+  catch(err) {
+    callback(err, resultList);
+  }
 };
 
 module.exports = exporter
